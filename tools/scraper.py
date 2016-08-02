304d650d19d5be3d76d7f3d8b41f3fd64e25b439
Raphaël Vinot <raphael@vinot.info>
2016-08-02 12:08:58 +0200

    Make scrapper python3 compatible

--- tools/scraper.py
+++ tools/scraper.py
@@ -5,13 +5,13 @@ A scraper which pulls zone WHOIS servers from IANA's root zone database.
 import csv
 import logging
 import socket
-import StringIO
+import io
 import sys
 import time
-import urlparse
+import urllib.parse
 
-import beautifulscraper
-
+from bs4 import BeautifulSoup
+import requests
 
 ROOT_ZONE_DB = 'http://www.iana.org/domains/root/db'
 SLEEP = 0
@@ -19,11 +19,11 @@ SLEEP = 0
 IP_ASSIGNATIONS = 'https://www.iana.org/assignments/ipv4-address-space/ipv4-address-space.csv'
 
 
-def ip_assignations(scraper):
+def ip_assignations():
     logging.info("Scraping %s", IP_ASSIGNATIONS)
-    assignations = scraper.go(IP_ASSIGNATIONS)
-    print assignations
-    reader = csv.reader(StringIO.StringIO(assignations))
+    assignations = requests.get(IP_ASSIGNATIONS).text
+    print(assignations)
+    reader = csv.reader(io.StringIO(assignations))
     next(reader, None)
     no_server = []
     for p, designation, date, whois_server, status, note in reader:
@@ -33,9 +33,9 @@ def ip_assignations(scraper):
             no_server.append(prefix)
         else:
             logging.info("WHOIS server for %s is %s", prefix, whois_server)
-            print '%s=%s' % (prefix, whois_server)
+            print('%s=%s' % (prefix, whois_server))
     for prefix in no_server:
-        print '; No record for %s' % prefix
+        print('; No record for %s' % prefix)
 
 
 def main():
@@ -45,14 +45,14 @@ def main():
     """
     logging.basicConfig(stream=sys.stderr, level=logging.INFO)
 
-    print '[overrides]'
+    print('[overrides]')
 
     logging.info("Scraping %s", ROOT_ZONE_DB)
-    scraper = beautifulscraper.BeautifulScraper()
-    body = scraper.go(ROOT_ZONE_DB)
+    zone_page = requests.get(ROOT_ZONE_DB).text
+    soup = BeautifulSoup(zone_page, 'html.parser')
 
     no_server = []
-    for link in body.select('#tld-table .tld a'):
+    for link in soup.select('#tld-table .tld a'):
         if 'href' not in link.attrs:
             continue
 
@@ -65,9 +65,10 @@ def main():
 
         time.sleep(SLEEP)
 
-        zone_url = urlparse.urljoin(ROOT_ZONE_DB, link.attrs['href'])
+        zone_url = urllib.parse.urljoin(ROOT_ZONE_DB, link.attrs['href'])
         logging.info("Scraping %s", zone_url)
-        body = scraper.go(zone_url)
+        b = requests.get(zone_url).text
+        body = BeautifulSoup(b, 'html.parser')
 
         title = body.find('h1')
         if title is None:
@@ -77,7 +78,7 @@ def main():
         if len(title_parts) != 2:
             logging.info("Could not find TLD in '%s'", title)
             continue
-        ace_zone = title_parts[1].encode('idna').lower()
+        ace_zone = title_parts[1].encode('idna').decode().lower()
 
         whois_server_label = body.find('b', text='WHOIS Server:')
         whois_server = ''
@@ -98,10 +99,10 @@ def main():
             no_server.append(ace_zone)
         else:
             logging.info("WHOIS server for %s is %s", ace_zone, whois_server)
-            print '%s=%s' % (ace_zone, whois_server)
+            print('%s=%s' % (ace_zone, whois_server))
 
     for ace_zone in no_server:
-        print '; No record for %s' % ace_zone
+        print('; No record for %s' % ace_zone)
 
     logging.info("Done")
 
