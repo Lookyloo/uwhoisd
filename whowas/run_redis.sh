d83dd166f059549f6a928782052c2bcb9866a13c
Raphaël Vinot <raphael@vinot.info>
2024-10-17 17:55:51 +0200

    fix: use the right config file...

--- whowas/run_redis.sh
+++ whowas/run_redis.sh
@@ -4,10 +4,10 @@ set -e
 set -x
 
 if [ -f  ../../valkey/src/valkey-server ]; then
-    ../../valkey/src/valkey-server ./cache.conf
+    ../../valkey/src/valkey-server ./whowas.conf
 elif [ -f ../../redis/src/redis-server ]; then
-    ../../redis/src/redis-server ./cache.conf
+    ../../redis/src/redis-server ./whowas.conf
 else
     echo "Warning: using system redis-server. Valkey-server or redis-server from source is recommended." >&2
-    /usr/bin/redis-server ./cache.conf
+    /usr/bin/redis-server ./whowas.conf
 fi
